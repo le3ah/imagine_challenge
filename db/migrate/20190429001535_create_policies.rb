185560674d31607549a2d661586b67cf8fbb4e2f
Leah Miller <lkmiller.87@gmail.com>
2019-04-28 19:15:08 -0600

    Adds attributes to policy table

--- db/migrate/20190429001535_create_policies.rb
+++ db/migrate/20190429001535_create_policies.rb
@@ -3,6 +3,12 @@ class CreatePolicies < ActiveRecord::Migration[5.2]
     create_table :policies do |t|
       t.references :carrier, foreign_key: true
       t.references :client, foreign_key: true
+      t.string :type, :null => true
+      t.string :division, :null => true
+      t.string :effective_date, :null => true
+      t.string :expiration_date, :null => true
+      t.string :written_premium, :null => true
+      t.string :carrier_policy_number, :null => true
     end
   end
 end
