c20f0659666090919c0a69d2bd5104983e4dc2a3
Leah Miller <lkmiller.87@gmail.com>
2019-04-29 13:06:46 -0600

    Adds index method for the policies search controller

--- app/controllers/api/v1/policies/search_controller.rb
+++ app/controllers/api/v1/policies/search_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::Policies::SearchController < ApplicationController
       render json: PolicySerializer.new(Policy.find_by(carrier_policy_number: params[:carrier_policy_number]))
     end
   end
+
+  def index
+    if params[:expiration_date]
+      render json: PolicySerializer.new(Policy.where(expiration_date: params[:expiration_date]))
+    end
+  end
 end
