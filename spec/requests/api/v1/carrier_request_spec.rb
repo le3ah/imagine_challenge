2bd0510506261a421485f396218b61c814c56a36
Leah Miller <lkmiller.87@gmail.com>
2019-04-29 09:26:16 -0600

    Adds carrier api request test setup for index page

---
+++ spec/requests/api/v1/carrier_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Carrier API' do
+  it "displays a list of carriers" do
+    create_list(:carrier, 3)
+    get '/api/v1/carriers'
+
+    expect(response).to be_successful
+
+    carriers = JSON.parse(response.body)
+
+    expect(carriers.count).to eq(1)
+    binding.pry
+    expect(carriers["data"].count).to eq(3)
+  end
+end
