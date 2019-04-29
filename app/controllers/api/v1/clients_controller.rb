7f1aac858e27ea41f55b52d3e9298764e3fa417e
Leah Miller <lkmiller.87@gmail.com>
2019-04-29 17:07:25 -0600

    Adds pagination header method

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ClientsController < ApplicationController
   def index
-    render json: ClientSerializer.new(Client.page(page).per(per_page))
+    @clients = Client.page(page).per(per_page)
+    render json: ClientSerializer.new(@clients)
+    set_pagination_header
   end
 
   def show
@@ -15,5 +17,9 @@ class Api::V1::ClientsController < ApplicationController
 
   def per_page
     @per_page ||= params[:per_page] || 50
+  end
+
+  def set_pagination_header
+    headers["X-Total-Count"] = @clients.total_count
   end
 end
