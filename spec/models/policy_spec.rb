70d5c63b4f2ba7955ac392669ad9630ff8429cd2
Leah Miller <lkmiller.87@gmail.com>
2019-04-29 12:39:15 -0600

    Adds uniqueness validation test for policy numbers

--- spec/models/policy_spec.rb
+++ spec/models/policy_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Policy, type: :model do
     it { should belong_to :carrier }
     it { should belong_to :client }
   end
+  describe 'uniqueness validation' do
+    it { should validate_uniqueness_of(:carrier_policy_number) }
+  end 
 end
