da71555fcd5842a4fac377b8a0525d6b4ef5cd98
Leah Miller <lkmiller.87@gmail.com>
2019-04-29 16:44:31 -0600

    Adds pagination to client serializer index method

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::ClientsController < ApplicationController
   def index
-    render json: ClientSerializer.new(Client.all)
+    render json: ClientSerializer.new(Client.page(page).per(per_page))
   end
 
   def show
     render json: ClientSerializer.new(Client.find(params[:id]))
+  end
+
+  private
+
+  def page
+    @page ||= params[:page] || 1
+  end
+
+  def per_page
+    @per_page ||= params[:per_page] || 50
   end
 end
