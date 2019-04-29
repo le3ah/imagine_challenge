eef15da0271ef8c791ef674c551b2604f560205e
Leah Miller <lkmiller.87@gmail.com>
2019-04-29 10:37:18 -0600

    Adds namespaced index route for policies

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :carriers, only: [:index]
       resources :clients, only: [:index]
+      resources :policies, only: [:index]
     end
   end
 end
