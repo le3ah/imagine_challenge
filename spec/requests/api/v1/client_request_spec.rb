0b05e3a20989c0a871f431ef7414c87585360eea
Leah Miller <lkmiller.87@gmail.com>
2019-04-29 11:44:43 -0600

    Adds test setup for searching client by name

--- spec/requests/api/v1/client_request_spec.rb
+++ spec/requests/api/v1/client_request_spec.rb
@@ -12,4 +12,17 @@ describe 'Client API' do
     expect(clients.count).to eq(1)
     expect(clients["data"].count).to eq(3)
   end
+  context 'paramter find search' do
+    it "can find a single client by name" do
+      client_1 = create(:client)
+      client_2 = create(:client, name: "Lucille 3")
+
+      get "/api/v1/clients/find?name=#{client_1.name}"
+
+      client = JSON.parse(response.body)
+      expect(response).to be_successful
+      expect(client["data"]["attributes"]["name"]).to eq(client_1.name)
+      expect(client["data"]["attributes"]["name"]).to_not eq(client_2.name)
+    end
+  end
 end
